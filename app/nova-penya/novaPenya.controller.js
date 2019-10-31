273e199ba21bc5ae851259d74ad7b3e90ccedc7c
jotaela <joanlluisfabra@gmail.com>
2019-10-31 13:00:02 +0100

    WIP

--- app/nova-penya/novaPenya.controller.js
+++ app/nova-penya/novaPenya.controller.js
@@ -1,7 +1,6 @@
 angular.module('novaPenya')
     .controller('novaPenyaController', function novaPenyaController($scope, $mdBottomSheet, postPenyes) {
         $scope.submitForm = (isValid) => {
-            console.log('hola');
             if (isValid) {
                 postPenyes.post($scope.novaPenya);
                 $mdBottomSheet.hide();
